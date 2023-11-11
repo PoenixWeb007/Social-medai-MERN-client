d4a02f5ddafd3fcd8a307e3d4e922b17c126b449
soufiane El Kahlaoui <s.elkahlaoui007@gmail.com>
2023-11-11 22:07:08 +0100

    configure theme settings ans theme provider for MUI + routing systeme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useMemo } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./containers/homePage";
 import LoginPage from "./containers/loginPage";
 import ProfilePage from "./containers/profilPage";
 import Navbar from "./containers/navbar";
+import { useSelector } from "react-redux";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
 export const token = 1;
 export const websiteName = "Social Media";
+
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const token = useSelector((state) => state.token);
+  console.log(token);
   return (
     <div className="App">
       <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:id" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={token ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:id"
+              element={token ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
--- src/containers/loginPage/index.jsx
+++ src/containers/loginPage/index.jsx
@@ -5,96 +5,9 @@ import loginSchemas from "../../validationSchemas/login";
 import "./loginPage.css";
 import { useSelector } from "react-redux";
 
-/** 
 function LoginPage() {
   console.log("hello");
   return <div>LoginPage</div>;
 }
-*/
-
-const LoginPage = function () {
-  const token = useSelector((state) => state.token);
-  const handleSubmit = async (values, { setSubmitting }) => {
-    /*
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 1000);
-    */
-    const response = await fetch(`http://localhost:4000/auth/login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-
-      body: JSON.stringify(values),
-    });
-    const islogged = await response.json();
-    console.log(JSON.stringify(values), islogged);
-  };
-  return (
-    <>
-      <div className=" d-flex align-items-center justify-content-center">
-        <Formik
-          initialValues={{ email: "", password: "" }}
-          validationSchema={loginSchemas}
-          onSubmit={handleSubmit}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form
-              className="d-flex flex-column align-items-center "
-              style={{ width: "400px" }}
-              onSubmit={handleSubmit}
-            >
-              <span className="m-4">welcome to {websiteName}</span>
-              <div className="my-3 w-75">
-                <label htmlFor="email">Email</label>
-                <input
-                  id="email"
-                  className="form-control "
-                  type="email"
-                  name="email"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                />
-                {errors.email && touched.email && (
-                  <div className="form-text">{errors.email} </div>
-                )}
-              </div>
-              <div className="my-3 w-75">
-                <label htmlFor="password">Mot de passe</label>
-                <input
-                  id="password"
-                  className="form-control "
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                />
-                {errors.password && touched.password && errors.password}
-              </div>
-              <button
-                className="btn btn-primary m-4"
-                type="submit"
-                disabled={isSubmitting}
-              >
-                Submit
-              </button>
-            </form>
-          )}
-        </Formik>
-      </div>
-    </>
-  );
-};
 
 export default LoginPage;
